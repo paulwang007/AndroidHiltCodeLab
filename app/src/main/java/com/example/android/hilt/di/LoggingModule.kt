4823c628109eaad65c901bc5d8bf935948c3ac84
Paul Wang <paul.wang.is.007@gmail.com>
2021-08-24 09:45:06 -0700

    Add Qualifiers for multibinding.

---
+++ app/src/main/java/com/example/android/hilt/di/LoggingModule.kt
@@ -0,0 +1,39 @@
+package com.example.android.hilt.di
+
+import com.example.android.hilt.data.LoggerDataSource
+import com.example.android.hilt.data.LoggerInMemoryDataSource
+import com.example.android.hilt.data.LoggerLocalDataSource
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+
+@Qualifier
+annotation class InMemoryLogger
+
+@Qualifier
+annotation class DatabaseLogger
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class LoggingModule {
+
+    @Singleton
+    @Binds
+    @DatabaseLogger
+    abstract fun bindDatabaseLogger(impl: LoggerLocalDataSource): LoggerDataSource
+}
+
+@InstallIn(ActivityComponent::class)
+@Module
+abstract class LoggingInMemoryModule {
+    @ActivityScoped
+    @Binds
+    @InMemoryLogger
+    abstract fun bindInMemoryLogger(impl: LoggerInMemoryDataSource): LoggerDataSource
+}
