7ef73442a7e846cb71afc58e12a821382adea156
Paul Wang <paul.wang.is.007@gmail.com>
2021-08-23 21:55:20 -0700

    Add @Inject

--- app/src/main/java/com/example/android/hilt/util/DateFormatter.kt
+++ app/src/main/java/com/example/android/hilt/util/DateFormatter.kt
@@ -18,16 +18,17 @@ package com.example.android.hilt.util
 
 import android.annotation.SuppressLint
 import java.text.SimpleDateFormat
-import java.util.Date
+import java.util.*
+import javax.inject.Inject
 
 /**
  * String formatter for the log dates.
  */
-class DateFormatter {
+class DateFormatter @Inject constructor() {
 
     @SuppressLint("SimpleDateFormat")
     private val formatter = SimpleDateFormat("d MMM yyyy HH:mm:ss")
-
+    
     fun formatDate(timestamp: Long): String {
         return formatter.format(Date(timestamp))
     }
